9d2dc33d357ff697842dff1f6a06c063755e31b6
郭强 <guoqiang@guoqiangdeMacBook-Pro.local>
2019-11-05 17:40:21 +0800

    add method pre_develop_environment

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #! /bin/bash
 
-#检查是否存在~/.zhsrc 文件
-function check_oh_my_zsh() {
-    # read file
+function install_oh_my_zsh() {
 
     if [ -d ~/.oh-my-zsh ] then
-        echo 'zshrc配置文件存在'
+        echo 'You have already Oh My Zsh installed'
     else
-        echo 'zshrc配置文件不存在,开始安装oh-my-zsh'
-         install_oh_my_zsh
+        echo 'trying to install oh-my-zsh...'
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     fi
 }
 
-#安装oh-my-zsh
-function install_oh_my_zsh() {
-    echo "******* installing oh-my-zsh ********"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+function install_cocoapods() {
+    if [ ! `which pod`] then 
+        echo 'trying to install cocoapods...'
+            sudo gem install cocoapods
+    else
+        echo 'You have already cocoapods installed'
+    fi
+
 }
 
 
@@ -23,14 +26,30 @@ function install_homebrew() {
 
     if [ ! `which brew`] then 
         echo 'trying to install homebrew...'
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
     else
         echo 'You have already homebrew installed'
     fi
         
 }
 
+function install_react_native() {
+    brew install watchman
+    # use nvm to manage node 
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+
+    nvm install node 
+
+    npm install -g yarn react-native-cli
+}
 
 
-check_oh_my_zsh
+function pre_develop_environment() {
+
+install_oh_my_zsh
+install_cocoapods
+install_homebrew
+install_react_native
+
+
+}
