b762255275f35ca5e15570dbc02d2184f0427ab6
郭强 <guoqiang@guoqiangdeMacBook-Pro.local>
2019-11-05 15:38:07 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,13 @@
 
 #检查是否存在~/.zhsrc 文件
 function check_oh_my_zsh() {
-
-    file="~/.zshrc"
     # read file
 
-    if [ -f "$file" ]
-    then
+    if [ -d ~/.oh-my-zsh ] then
         echo 'zshrc配置文件存在'
     else
-        echo 'zshrc配置文件不存在******'
-        # install_oh_my_zsh
-        # check_oh_my_zsh
+        echo 'zshrc配置文件不存在,开始安装oh-my-zsh'
+         install_oh_my_zsh
     fi
 }
 
@@ -21,5 +17,20 @@ function install_oh_my_zsh() {
     echo "******* installing oh-my-zsh ********"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
+
+
+function install_homebrew() {
+
+    if [ ! `which brew`] then 
+        echo 'trying to install homebrew...'
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        
+    else
+        echo 'You have already homebrew installed'
+    fi
+        
+}
+
+
 
 check_oh_my_zsh
